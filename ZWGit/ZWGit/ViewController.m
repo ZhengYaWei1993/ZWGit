933b8cf5207770a3818310671212bd094234c780
郑亚伟 <zhengyawei@ZYWMacBook-Air.local>
2016-08-30 15:30:12 +0800

    //现在已经下班了  我在自己新建的分支上提交了代码  提交代码的时候完全退出XCode

--- ZWGit/ZWGit/ViewController.m
+++ ZWGit/ZWGit/ViewController.m
@@ -19,6 +19,7 @@
     // Do any additional setup after loading the view, typically from a nib.
     //2
     //3
+    //现在已经下班了  我在自己新建的分支上提交了代码  提交代码的时候完全退出XCode
 }
 
 - (void)didReceiveMemoryWarning {
